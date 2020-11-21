fc9bc1cfd6314e19b39bef3093b1706372bc7a53
adam1279 <adamdamgolan@gmail.com>
2020-11-21 17:36:44 +0100

    idk

---
+++ The Royal Garden/Assets/Follow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform player;
+    public float cameraDistance = 30.0f;
+    void Start()
+    {
+        GetComponent<UnityEngine.Camera>().orthographicSize = ((Screen.height / 2) / cameraDistance);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);
+    }
+}
---
+++ The Royal Garden/Assets/movement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public float playerSpeed = 2.0f;
+    private float pixel = 1;
+    private bool isColliding;
+    //private CharacterController controller;
+
+    private void Start()
+    {
+        //controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        float x = 0;
+        float y = 0;
+        if (Input.GetKey("left"))
+        {
+            x = -1;
+        }
+        if (Input.GetKey("right"))
+        {
+            x = 1;
+        }
+        if (Input.GetKey("up"))
+        {
+            y = 1;
+        }
+        if (Input.GetKey("down"))
+        {
+            y = -1;
+        }
+        if (isColliding) {
+            x = 0;
+            y = 0;
+        }
+        Vector2 move = new Vector2(x * pixel, y * pixel);
+        //controller.Move(move * Time.deltaTime * playerSpeed);
+        transform.Translate(move * Time.deltaTime * playerSpeed);
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        isColliding = true;
+        print("collision");
+    }
+}
